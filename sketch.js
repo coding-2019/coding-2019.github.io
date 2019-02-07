85817b08c8d22b1c12a48115dd34222dde994de9
coding-2019 <47307983+coding-2019@users.noreply.github.com>
2019-02-07 09:36:26 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
 var button = document.createElement('button');
-button.innerText = "click";
+button.innerText = "click for a quiz!";
 button.onclick = () => {
-    alert("clicked!");
+    var score = 0;
+    var q1 = prompt("what is 2+2*2")
+    if(q1 == 6) score++;
+    var q2 = prompt("what is 3!");
+    if(q2 == 6) score++;
+    var q3 = prompt("how many meters are in a hectometer?");
+    if(q3 == 100) score++;
+    alert(score/3*10+"%");
 }
 document.body.appendChild(button);
